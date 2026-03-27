8a25341e5ece4b706f7c2c1d7a453961ebed5bb7
pollinnshh <polinashvch248@gmail.com>
2026-03-27 17:03:08 +0300

    add iters

--- shevchenko.polina/S2/main.cpp
+++ shevchenko.polina/S2/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
     input = &file;
   }
 
-  Stack< ll_t > results;
+  List<ll_t> results;
   string line;
 
   try
@@ -31,13 +31,10 @@ int main(int argc, char* argv[])
     Calculator calc;
     while (getline(*input, line))
     {
-      if (line.empty())
-      {
-        continue;
-      }
+      if (line.empty()) continue;
 
       ll_t result = calc.evaluate(line);
-      results.push(result);
+      results.pushBack(result);
     }
 
     if (results.empty())
@@ -46,23 +43,24 @@ int main(int argc, char* argv[])
       return 0;
     }
 
-    Stack<ll_t> reversed;
-    while (!results.empty())
-    {
-      reversed.push(results.pop());
-    }
-
     bool first = true;
-    while (!reversed.empty())
+    auto it = results.last();
+    while (true)
     {
       if (!first)
       {
         cout << " ";
       }
-      cout << reversed.pop();
+      cout << *it;
       first = false;
+
+      if (it == results.begin())
+      {
+        break;
+      }
+      --it;
     }
-    cout << endl;
+    cout << "\n";
   }
   catch (const overflow_error& e)
   {
@@ -81,7 +79,7 @@ int main(int argc, char* argv[])
   }
   catch (...)
   {
-    cerr << "unknown error" << endl;
+    cerr << "unknown error\n";
     return 1;
   }
 
