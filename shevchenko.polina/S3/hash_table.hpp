b3556b7055d1e8c99aefa032b3f20dc8d4330110
pollinnshh <polinashvch248@gmail.com>
2026-05-16 19:36:49 +0300

    add partly HashTable with overflow exception

--- shevchenko.polina/S3/hash_table.hpp
+++ shevchenko.polina/S3/hash_table.hpp
@@ -1,153 +1,219 @@
 #ifndef HASH_TABLE_HPP
 #define HASH_TABLE_HPP
 
-#include <cstddef>
-#include <utility>
 #include <functional>
 #include <stdexcept>
+#include <utility>
 
-namespace shevchenko {
+#include "list.hpp"
 
-template<typename Key, typename Value>
-struct BucketNode {
-  Key key;
-  Value value;
-  BucketNode* next;
+namespace shevchenko
+{
+template< class Key, class Value >
+struct HashPair
+{
+  Key first;
+  Value second;
   
-  BucketNode(const Key& k, const Value& v, BucketNode* n = nullptr)
-  : key(k)
-  , value(v)
-  , next(n) {
+  HashPair():
+  first(),
+  second()
+  {
+  }
+  
+  HashPair(const Key& key, const Value& value):
+  first(key),
+  second(value)
+  {
   }
 };
 
-template<typename Key, typename Value, typename Hash, typename Equal>
-class HashTable;
-
-template<typename Key, typename Value, typename Hash, typename Equal>
-class HashTableIterator {
-private:
-  friend class HashTable<Key, Value, Hash, Equal>;
+template<class Key, class Value, class Hash, class Equal = std::equal_to< Key>>
+class HashTable
+{
+public:
+  using value_type = HashPair< Key, Value >;
+  using bucket_type = List< value_type >;
   
-  HashTableIterator(typename HashTable<Key, Value, Hash, Equal>::node_type** buckets, size_t bucket_count, size_t bucket_index, typename HashTable<Key, Value, Hash, Equal>::node_type* node)
-  : buckets_(buckets)
-  , bucket_count_(bucket_count)
-  , bucket_index_(bucket_index)
-  , node_(node) {
+  explicit HashTable( size_t bucket_count = , size_t bucket_size = ):
+  bucket_count_(bucket_count),
+  bucket_size_(bucket_size),
+  size_(0),
+  buckets_(new bucket_type[bucket_count])
+  {
   }
-
-  typename HashTable<Key, Value, Hash, Equal>::node_type** buckets_;
-  size_t bucket_count_;
-  size_t bucket_index_;
-  typename HashTable<Key, Value, Hash, Equal>::node_type* node_;
   
-public:
-  
-};
-
-template<typename Key, typename Value,
-typename Hash = std::hash<Key>,
-typename Equal = std::equal_to<Key>>
-class HashTable {
-public:
-  using key_type = Key;
-  using mapped_type = Value;
-  using hasher = Hash;
-  using key_equal = Equal;
-  using node_type = BucketNode<Key, Value>;
-  using iterator = HashTableIterator<Key, Value, Hash, Equal>;
-
-  explicit HashTable(size_t initial_buckets = 16)
-  : bucket_count_(initial_buckets > 0 ? initial_buckets : 16)
-  , size_(0)
-  , buckets_(new node_type*[bucket_count_]())
-  , hash_()
-  , equal_() {
-    for (size_t i = 0; i < bucket_count_; ++i) {
-      buckets_[i] = nullptr;
+  HashTable(const HashTable& other):
+  bucket_count_(other.bucket_count_),
+  bucket_size_(other.bucket_size_),
+  size_(other.size_),
+  buckets_(new bucket_type[other.bucket_count_])
+  {
+    for (size_t i = 0; i < bucket_count_; ++i)
+    {
+      buckets_[i] = other.buckets_[i];
     }
   }
-
-  ~HashTable() {
-    clear();
-    delete[] buckets_;
-  }
-
-  HashTable(const HashTable&) = delete;
-  HashTable& operator=(const HashTable&) = delete;
-
-  HashTable(HashTable&& other) noexcept
-  : bucket_count_(other.bucket_count_)
-  , size_(other.size_)
-  , buckets_(other.buckets_)
-  , hash_(std::move(other.hash_))
-  , equal_(std::move(other.equal_)) {
-    other.buckets_ = nullptr;
-    other.bucket_count_ = 0;
-    other.size_ = 0;
-  }
   
-  HashTable& operator=(HashTable&& other) noexcept {
-    if (this != &other) {
-      clear();
-      delete[] buckets_;
-      bucket_count_ = other.bucket_count_;
-      size_ = other.size_;
-      buckets_ = other.buckets_;
-      hash_ = std::move(other.hash_);
-      equal_ = std::move(other.equal_);
-      other.buckets_ = nullptr;
-      other.bucket_count_ = 0;
-      other.size_ = 0;
+  HashTable& operator=(const HashTable& other)
+  {
+    if (this != &other)
+    {
+      HashTable tmp(other);
+      swap(tmp);
     }
+    
     return *this;
   }
-
-  void clear() noexcept {
-    for (size_t i = 0; i < bucket_count_; ++i) {
-      node_type* current = buckets_[i];
-      while (current != nullptr) {
-        node_type* next = current->next;
-        delete current;
-        current = next;
-      }
-      buckets_[i] = nullptr;
-    }
-    size_ = 0;
-  }
-
-  void add(const Key& key, const Value& value) {
-
-    (void)key;
-    (void)value;
+  
+  ~HashTable()
+  {
+    delete[] buckets_;
   }
   
-  bool has(const Key& key) const {
-
-    (void)key;
-    return false;
-  }
-  
-  size_t size() const noexcept {
-    return size_;
-  }
-  
-  bool empty() const noexcept {
+  bool empty() const
+  {
     return size_ == 0;
   }
   
-  size_t bucket_count() const noexcept {
-    return bucket_count_;
+  size_t size() const
+  {
+    return size_;
+  }
+  
+  bool contains(const Key& key) const
+  {
+    size_t index = getIndex(key);
+    
+    for (auto it = buckets_[index].begin();
+         it != buckets_[index].end();
+         ++it)
+    {
+      if (equal_((*it).first, key))
+      {
+        return true;
+      }
+    }
+    
+    return false;
+  }
+  
+  void insert(const Key& key, const Value& value)
+  {
+    size_t index = getIndex(key);
+    
+    bucket_type& bucket = buckets_[index];
+    
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (equal_((*it).first, key))
+      {
+        (*it).second = value;
+        return;
+      }
+    }
+    
+    if (bucket.size() >= bucket_size_)
+    {
+      throw std::overflow_error("bucket overflow");
+    }
+    
+    bucket.pushBack(value_type(key, value));
+    ++size_;
+  }
+  
+  Value& at(const Key& key)
+  {
+    size_t index = getIndex(key);
+    
+    bucket_type& bucket = buckets_[index];
+    
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (equal_((*it).first, key))
+      {
+        return (*it).second;
+      }
+    }
+    
+    throw std::out_of_range("key not found");
+  }
+  
+  const Value& at(const Key& key) const
+  {
+    size_t index = getIndex(key);
+    
+    const bucket_type& bucket = buckets_[index];
+    
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (equal_((*it).first, key))
+      {
+        return (*it).second;
+      }
+    }
+    
+    throw std::out_of_range("key not found");
+  }
+  
+  void erase(const Key& key)
+  {
+    size_t index = getIndex(key);
+    bucket_type& bucket = buckets_[index];
+    
+    for (auto it = bucket.begin(); it != bucket.end(); ++it)
+    {
+      if (equal_((*it).first, key))
+      {
+        bucket.erase(it);
+        --size_;
+        return;
+      }
+    }
+    
+    throw std::out_of_range("key not found");
+  }
+  
+  void rehash(size_t new_bucket_count)
+  {
+    HashTable tmp(new_bucket_count, bucket_size_);
+    
+    for (size_t i = 0; i < bucket_count_; ++i)
+    {
+      for (auto it = buckets_[i].begin();
+           it != buckets_[i].end();
+           ++it)
+      {
+        tmp.insert((*it).first, (*it).second);
+      }
+    }
+    
+    swap(tmp);
+  }
+  
+  void swap(HashTable& other) noexcept
+  {
+    std::swap(bucket_count_, other.bucket_count_);
+    std::swap(bucket_size_, other.bucket_size_);
+    std::swap(size_, other.size_);
+    std::swap(buckets_, other.buckets_);
   }
   
 private:
+  size_t getIndex(const Key& key) const
+  {
+    return hash_(key) % bucket_count_;
+  }
+  
   size_t bucket_count_;
+  size_t bucket_size_;
   size_t size_;
-  node_type** buckets_;
+  
+  bucket_type* buckets_;
+  
   Hash hash_;
   Equal equal_;
 };
-
 }
 
 #endif
