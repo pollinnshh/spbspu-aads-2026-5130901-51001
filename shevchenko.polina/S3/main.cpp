9adff665002ca2fb776015412f747f320c4c9fbd
pollinnshh <polinashvch248@gmail.com>
2026-05-16 16:01:52 +0300

    add C++ for SHA-1 and SHA1Hasher

--- shevchenko.polina/S3/main.cpp
+++ shevchenko.polina/S3/main.cpp
@@ -1,25 +1,30 @@
-#include "sha1_c.h"
+#include "sha1.hpp"
 #include <iostream>
 #include <iomanip>
-#include <cstring>
 
 int main() {
-  // "abc" из RFC (a9993e364706816aba3e25717850c26c9cd0d89d)
-  SHA1Context ctx;
-  SHA1Reset(&ctx);
+
+  shevchenko::SHA1 sha;
+  sha.update("abc");
+  auto digest = sha.final();
   
-  const char* test = "abc";
-  SHA1Input(&ctx, (const uint8_t*)test, strlen(test));
-  
-  uint8_t digest[20];
-  SHA1Result(&ctx, digest);
-  
-  std::cout << "SHA-1(\"abc\") = ";
-  for (int i = 0; i < 20; i++) {
+  std::cout << "SHA-1(\"abc\") via class = ";
+  for (unsigned char c : digest) {
     std::cout << std::hex << std::setw(2) << std::setfill('0')
-    << (int)digest[i];
+    << (int)c;
   }
-  std::cout << std::endl;
+  std::cout << "\n";
+  
+  auto hash2 = shevchenko::sha1_hash("abc");
+  std::cout << "SHA-1(\"abc\") via function = ";
+  for (unsigned char c : hash2) {
+    std::cout << std::hex << std::setw(2) << std::setfill('0')
+    << (int)c;
+  }
+  std::cout << "\n";
+
+  shevchenko::SHA1Hasher hasher;
+  std::cout << "Hash of \"abc\" as size_t = " << hasher("abc") << "\n";
   
   return 0;
 }
