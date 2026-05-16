b002436eaa03f73d24cc5f28c62cbf46d5ab2fa0
pollinnshh <polinashvch248@gmail.com>
2026-05-16 15:54:53 +0300

    add SHA-1 implementation from RFC 3174

--- shevchenko.polina/S3/main.cpp
+++ shevchenko.polina/S3/main.cpp
@@ -1,8 +1,25 @@
-#include "sha1.hpp"
+#include "sha1_c.h"
 #include <iostream>
+#include <iomanip>
+#include <cstring>
 
 int main() {
-  auto hash = shevchenko::sha1_hash("abc");
-  std::cout << "SHA-1 hash size: " << hash.size() << "\n";
+  // "abc" из RFC (a9993e364706816aba3e25717850c26c9cd0d89d)
+  SHA1Context ctx;
+  SHA1Reset(&ctx);
+  
+  const char* test = "abc";
+  SHA1Input(&ctx, (const uint8_t*)test, strlen(test));
+  
+  uint8_t digest[20];
+  SHA1Result(&ctx, digest);
+  
+  std::cout << "SHA-1(\"abc\") = ";
+  for (int i = 0; i < 20; i++) {
+    std::cout << std::hex << std::setw(2) << std::setfill('0')
+    << (int)digest[i];
+  }
+  std::cout << std::endl;
+  
   return 0;
 }
