36833a86da55bf0b81c95b8b09ccd7ffa23db0ff
pollinnshh <polinashvch248@gmail.com>
2026-05-23 17:15:20 +0300

    add in BSTree operations

--- shevchenko.polina/S4/bstree.hpp
+++ shevchenko.polina/S4/bstree.hpp
@@ -15,6 +15,24 @@ class BSTConstIterator;
 template< class Key, class Value, class Compare = std::less< Key > >
 class BSTree
 {
+private:
+  struct Node
+  {
+    std::pair< Key, Value > data;
+    
+    Node* parent;
+    Node* left;
+    Node* right;
+    
+    Node(const Key& key, const Value& value, Node* par):
+    data(key, value),
+    parent(par),
+    left(nullptr),
+    right(nullptr)
+    {
+    }
+  };
+  
 public:
   using iterator = BSTIterator< Key, Value >;
   using const_iterator = BSTConstIterator< Key, Value >;
@@ -50,5 +68,59 @@ public:
   size_t height(const_iterator it) const;
   
 private:
-
+  Node* fake_;
+  size_t size_;
+  Compare comp_;
+  
+  void clear(Node* node);
+  
 };
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::BSTree():
+fake_(new Node(Key(), Value(), nullptr)),
+size_(0),
+comp_(Compare())
+{
+}
+
+template< class Key, class Value, class Compare >
+BSTree< Key, Value, Compare >::~BSTree()
+{
+  clear();
+  delete fake_;
+}
+
+template< class Key, class Value, class Compare >
+bool BSTree< Key, Value, Compare >::empty() const noexcept
+{
+  return size_ == 0;
+}
+
+template< class Key, class Value, class Compare >
+size_t BSTree< Key, Value, Compare >::size() const noexcept
+{
+  return size_;
+}
+
+template< class Key, class Value, class Compare >
+void BSTree< Key, Value, Compare >::clear()
+{
+  clear(fake_->left);
+  fake_->left = nullptr;
+  size_ = 0;
+}
+
+template< class Key, class Value, class Compare >
+void BSTree< Key, Value, Compare >::clear(Node* node)
+{
+  if (node == nullptr)
+  {
+    return;
+  }
+  
+  clear(node->left);
+  clear(node->right);
+  
+  delete node;
+}
