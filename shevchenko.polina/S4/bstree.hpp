8f97e390c5f8f4c75ecf3292bcc0cec4bbd441ec
pollinnshh <polinashvch248@gmail.com>
2026-05-23 16:30:47 +0300

    create base BST struct and initialisation commands

--- shevchenko.polina/S4/bstree.hpp
+++ shevchenko.polina/S4/bstree.hpp
@@ -1,1 +1,54 @@
+#ifndef BSTREE_HPP
+#define BSTREE_HPP
 
+#include <cstddef>
+#include <utility>
+
+namespace shevchenko
+{
+template< class Key, class Value >
+class BSTIterator;
+
+template< class Key, class Value >
+class BSTConstIterator;
+
+template< class Key, class Value, class Compare = std::less< Key > >
+class BSTree
+{
+public:
+  using iterator = BSTIterator< Key, Value >;
+  using const_iterator = BSTConstIterator< Key, Value >;
+  
+  BSTree();
+  BSTree(const BSTree& other);
+  BSTree(BSTree&& other) noexcept;
+  ~BSTree();
+  BSTree& operator=(const BSTree& other);
+  BSTree& operator=(BSTree&& other) noexcept;
+  
+  void push(const Key& key, const Value& value);
+  Value get(const Key& key) const;
+  Value drop(const Key& key);
+  
+  bool empty() const noexcept;
+  
+  size_t size() const noexcept;
+  
+  iterator begin();
+  iterator end();
+  
+  const_iterator cbegin() const;
+  const_iterator cend() const;
+  
+  const_iterator rotateLeft(const_iterator it);
+  const_iterator rotateRight(const_iterator it);
+  
+  const_iterator rotateLargeLeft(const_iterator it);
+  const_iterator rotateLargeRight(const_iterator it);
+  
+  size_t height() const;
+  size_t height(const_iterator it) const;
+  
+private:
+
+};
--- shevchenko.polina/S4/commands.hpp
+++ shevchenko.polina/S4/commands.hpp
@@ -1,1 +1,20 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
 
+#include "bstree.hpp"
+
+namespace shevchenko
+{
+using Dictionary = BSTree< int, std::string >;
+using Dictionaries = BSTree< std::string, Dictionary >;
+
+void readDict();
+
+void doPrint();
+void doUnion();
+void doIntersect(Dictionaries& dictionaries, std::istream& input);
+void doComplement(Dictionaries& dictionaries, std::istream& input);
+
+}
+
+#endif
