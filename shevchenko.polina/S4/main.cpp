dc641cbf4f77db73e7f11cfe8c75fc3d5e30b489
pollinnshh <polinashvch248@gmail.com>
2026-05-24 18:39:49 +0300

    main.cpp complete

--- shevchenko.polina/S4/main.cpp
+++ shevchenko.polina/S4/main.cpp
@@ -1,4 +1,52 @@
 #include <fstream>
 #include <iostream>
+#include <limits>
 
-int main() {}
+#include "commands.hpp"
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "invalid arguments\n";
+    return 1;
+  }
+
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "cannot open file\n";
+    return 1;
+  }
+
+  shevchenko::Dictionaries dicts;
+  shevchenko::readDict(file, dicts);
+
+  std::string command;
+  while (std::cin >> command)
+  {
+    if (command == "print")
+    {
+      shevchenko::doPrint(dicts, std::cin, std::cout);
+    }
+    else if (command == "union")
+    {
+      shevchenko::doUnion(dicts, std::cin, std::cout);
+    }
+    else if (command == "intersect")
+    {
+      shevchenko::doIntersect(dicts, std::cin, std::cout);
+    }
+    else if (command == "complement")
+    {
+      shevchenko::doComplement(dicts, std::cin, std::cout);
+    }
+    else
+    {
+      output << "<INVALID COMMAND>\n";
+      input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+
+  return 0;
+}
