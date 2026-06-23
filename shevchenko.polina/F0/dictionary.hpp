74543911f7579f17a0c4cd703716e49e7466fc5c
pollinnshh <polinashvch248@gmail.com>
2026-06-23 15:53:17 +0300

    add user management

--- shevchenko.polina/F0/dictionary.hpp
+++ shevchenko.polina/F0/dictionary.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 
 namespace shevchenko
 {
@@ -15,9 +16,20 @@ struct Translation
   std::string pos;
 };
 
-class DictionarySystem
+struct User
+{
+  std::string name;
+  std::string assignedDict;
+};
+
+class LearningSystem
 {
 public:
+  LearningSystem()
+  {
+    srand(static_cast<unsigned int>(time(nullptr)));
+  }
+
   void makeDictionary(const std::string& name)
   {
     if (dictionaries_.contains(name))
@@ -83,6 +95,30 @@ public:
     std::cout << "<ADDED: " << eng << " (" << pos << ") - " << rus << ">\n";
   }
 
+  void addTranslation(const std::string& dictName, const std::string& eng,
+                      const std::string& rus, const std::string& pos)
+  {
+    if (!dictionaries_.contains(dictName))
+    {
+      std::cout << "<ERROR: Dictionary '" << dictName << "' not found>\n";
+      return;
+    }
+
+    BSTree<std::string, Translation>& dict = dictionaries_.at(dictName);
+
+    if (!dict.contains(eng))
+    {
+      std::cout << "<ERROR: Word '" << eng << "' not found>\n";
+      return;
+    }
+
+    Translation trans;
+    trans.russian = rus;
+    trans.pos = pos;
+    dict.insert(eng, trans);
+    std::cout << "<ADDED TRANSLATION: " << eng << " (" << pos << ") - " << rus << ">\n";
+  }
+
   void lookup(const std::string& dictName, const std::string& eng)
   {
     if (!dictionaries_.contains(dictName))
@@ -103,8 +139,79 @@ public:
     std::cout << eng << ": (" << trans.pos << ") " << trans.russian << "\n";
   }
 
+  void addUser(const std::string& username)
+  {
+    if (users_.contains(username))
+    {
+      std::cout << "<INVALID COMMAND: User '" << username << "' already exists>\n";
+      return;
+    }
+
+    User newUser;
+    newUser.name = username;
+    newUser.assignedDict = "";
+    users_.insert(username, newUser);
+    std::cout << "<USER CREATED: " << username << ">\n";
+  }
+
+  void login(const std::string& username)
+  {
+    if (!users_.contains(username))
+    {
+      std::cout << "<ERROR: User '" << username << "' not found>\n";
+      return;
+    }
+
+    currentUser_ = username;
+    std::cout << "<WELCOME BACK, " << username << ">\n";
+
+    User user = users_.at(username);
+    if (user.assignedDict.empty())
+    {
+      std::cout << "Assigned dictionary: none\n";
+    }
+    else
+    {
+      std::cout << "Assigned dictionary: " << user.assignedDict << "\n";
+    }
+  }
+
+  void logout()
+  {
+    if (currentUser_.empty())
+    {
+      std::cout << "<ERROR: No user logged in>\n";
+      return;
+    }
+
+    std::cout << "<USER LOGGED OUT: " << currentUser_ << ">\n";
+    currentUser_ = "";
+  }
+
+  void assignDict(const std::string& username, const std::string& dictName)
+  {
+    if (!users_.contains(username))
+    {
+      std::cout << "<ERROR: User '" << username << "' not found>\n";
+      return;
+    }
+
+    if (!dictionaries_.contains(dictName))
+    {
+      std::cout << "<ERROR: Dictionary '" << dictName << "' not found>\n";
+      return;
+    }
+
+    User user = users_.at(username);
+    user.assignedDict = dictName;
+    users_.insert(username, user);
+    std::cout << "<DICTIONARY ASSIGNED: " << dictName << " to " << username << ">\n";
+  }
+
 private:
   BSTree<std::string, BSTree<std::string, Translation>> dictionaries_;
+  BSTree<std::string, User> users_;
+  std::string currentUser_;
 };
 
 }
